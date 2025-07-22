e508e192f05863f2679b23287eb0b68aa6f090ed
Ubuntu <ubuntu@ip-172-31-29-236.ca-central-1.compute.internal>
2025-07-22 11:47:06 +0000

    fixed error where numerical was not equal to categorical size because of cleaning. Removed token counter (no longer using openai)

--- src/features.py
+++ src/features.py
@@ -5,10 +5,6 @@ from sklearn.preprocessing import MinMaxScaler
 def create_features(df):
     print("Creating Features.....")
     categorical = "Title: " + df.title + " Author: " + df.author + " Description: " + df.description + " Genres: " + df.genres 
-
-    categorical = categorical.dropna()
-    categorical = categorical[categorical.apply(check_tokens)]
-
     numerical = pd.DataFrame(df[['likedPercent', 'numRatings']]) 
 
     # scale the numerical features
