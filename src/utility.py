bcaeedac4ab9895207b89ab955fa74e52272107d
Ubuntu <ubuntu@ip-172-31-29-236.ca-central-1.compute.internal>
2025-07-22 20:44:29 +0000

    added s3 bucket upload function

--- src/utility.py
+++ src/utility.py
@@ -1,5 +1,9 @@
 import pandas as pd
+import boto3
+from io import StringIO
 
-
-def read_embeddings():
-    pd.read_csv()
+def upload_to_s3(df: pd.DataFrame, bucket_name: str, s3_key: str):
+    s3 = boto3.client("s3")
+    csv_buffer = StringIO()
+    df.to_csv(csv_buffer, index=False)
+    s3.put_object(Bucket=bucket_name, Key=s3_key, Body=csv_buffer.getvalue())
