1330a0e38e0bb616e854c489cc2dd34554e2ab19
Ubuntu <ubuntu@ip-172-31-29-236.ca-central-1.compute.internal>
2025-07-22 21:22:54 +0000

    changed upload and download to use parquet files

--- src/utility.py
+++ src/utility.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import boto3
-from io import StringIO
+from io import BytesIO
 
 def upload_to_s3(df: pd.DataFrame, bucket_name: str, s3_key: str):
     try:
         s3 = boto3.client("s3")
-        csv_buffer = StringIO()
-        df.to_csv(csv_buffer, index=False)
-        s3.put_object(Bucket=bucket_name, Key=s3_key, Body=csv_buffer.getvalue())
+        buffer = BytesIO()
+        df.to_csv(buffer, index=False)
+        s3.put_object(Bucket=bucket_name, Key=s3_key, Body=buffer.getvalue())
     except Exception as e:
         print(f"An exception has occured during upload: {e}")
 
@@ -17,8 +17,8 @@ def get_from_s3(bucket_name: str, file_key: str):
         response = s3.get_object(Bucket=bucket_name, Key=file_key)
         status_code = response['ResponseMetadata']['HTTPStatusCode']
         if status_code == 200:
-            csv_content = response['Body'].read().decode('utf-8')
-            return pd.read_csv(StringIO(csv_content))
+            parquet_contents = BytesIO(response['Body'].read())
+            return pd.read_parquet(parquet_contents)
         else:
             print(f"Error getting object: HTTP status code {status_code}")
             return None
